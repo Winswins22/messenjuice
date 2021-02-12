53a9b18cf1e6ca7449001b11778c068a5bd0fffa
Winswins22 <73000223+Winswins22@users.noreply.github.com>
2021-02-12 17:42:39 -0500

    Added comments + Removed testing code

--- locateMap.js
+++ locateMap.js
@@ -6,9 +6,11 @@ let data = {
 // Gets the location, then calls createMap()
 function getLocation (){
 
+    //console.log("Sending location request");
+
     navigator.geolocation.getCurrentPosition((position) => {
         
-        //console.log("Inside geolocator!");
+        //console.log("Location request response: Allowed");
 
         let lat;
         let long;
@@ -33,13 +35,16 @@ function getLocation (){
 // creates the map after getLocation() gets location data
 function createMap(){
 
+    // Create map
     let mymap = L.map('map').setView([data.lat, data.long], 13);
 
+    // Add attribution and data(tiles) to the map  ----------
     const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright"> OpenStreetMap</a> contributors';
     const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
     const tiles = L.tileLayer(tileUrl, { attribution });
     tiles.addTo(mymap);
+    // ------------------------------------------------------
 
     // Add marker at current location
     L.marker([data.lat, data.long]).addTo(mymap);
